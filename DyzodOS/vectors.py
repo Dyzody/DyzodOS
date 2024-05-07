b59d5358df48e216900b37f48a7ebef8d7e57648
Dyzody <david.schilcher11@gmx.at>
2024-05-07 14:27:24 +0200

    Cleaned up the Vectors Module

--- DyzodOS/vectors.py
+++ DyzodOS/vectors.py
@@ -1,4 +1,5 @@
 import math
+import numpy
 
 class Vector2D:
     def __init__(self, x, y):
@@ -9,33 +10,13 @@ class Vector2D:
         if isinstance(other, Vector2D):
             return Vector2D(self.x + other.x, self.y + other.y)
         else:
-            raise TypeError("Unsupported operand type. Vector2D can only be added to another Vector2D.")
+            return NotImplemented
 
     def __sub__(self, other):
         if isinstance(other, Vector2D):
             return Vector2D(self.x - other.x, self.y - other.y)
         else:
-            raise TypeError("Unsupported operand type. Vector2D can only be subtracted from another Vector2D.")
-
-    def get_length(self):
-        return math.sqrt(self.x*self.x + self.y*self.y)
-    
-    def showInText(self):
-        return f"X:{self.x} Y:{self.y}"
-
-    def vectorclamp(self, Vector):
-        if Vector.x > 0:
-            if self.x > Vector.x:
-                self.x = Vector.x
-        else:
-            if self.x < Vector.x:
-                self.x = Vector.x
-        if Vector.y > 0:
-            if self.y > Vector.y:
-                self.y = Vector.y
-        else:
-            if self.y < Vector.y:
-                self.y = Vector.y
+            return NotImplemented
 
     def __eq__(self, other):
         if isinstance(other, Vector2D):
@@ -52,8 +33,33 @@ class Vector2D:
             return self.get_length() > other.get_length()
         return NotImplemented
 
+    def length(self):
+        return math.sqrt(self.x**2 + self.y**2)
+
+    def vectorclamp(self, Vector):
+        if Vector.x > 0:
+            if self.x > Vector.x:
+                self.x = Vector.x
+        else:
+            if self.x < Vector.x:
+                self.x = Vector.x
+        if Vector.y > 0:
+            if self.y > Vector.y:
+                self.y = Vector.y
+        else:
+            if self.y < Vector.y:
+                self.y = Vector.y
+    
+    def InText(self):
+        return f"X:{self.x} Y:{self.y}"
+
+
 def normalizeVector(Vector, length):
-    current_magnitude = math.sqrt(Vector.x**2 + Vector.y**2)
+    
+    if not isinstance(Vector, Vector2D): 
+        return NotImplemented
+
+    current_magnitude = Vector.get_length()
         
     if current_magnitude == 0:
         return Vector2D(0, 0)
@@ -65,6 +71,10 @@ def normalizeVector(Vector, length):
     return normalized_vector
 
 def getaverageVector(Vec1, Vec2):
+    
+    if not isinstance(Vec1, Vector2D) or not isinstance(Vec2, Vector2D): 
+        return NotImplemented
+    
     averageX = (Vec1.x + Vec2.x)/2
     averageY = (Vec1.y + Vec2.y)/2
 
